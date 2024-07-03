90f1957e552b6dae531194d7fe2f0fddc82859db
Asrorhon Arslanov <asrorkhon.arslanov@mail.ru>
2024-07-03 17:18:20 +0500

    feat/feedback feature implemented

--- src/pages/dashboard-content.tsx
+++ src/pages/dashboard-content.tsx
@@ -1,6 +1,8 @@
 import ContentViewer from "@/components/dashboard/content-viewer";
 import { useContentContext } from "@/contexts/content.context";
 import { TGeneratedContent } from "@/shared/types/generated-content";
+import { StarIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -28,12 +30,35 @@ export default function DashboardContent(){
 		)
 	}
 
-	return <div>
+	const handleRateChange = (rate: number)=>{
+		if(generatedContent){
+			handleSave({
+				...generatedContent,
+				rate,
+			})
+		}
+	}
+
+	return (
+		<div>
+			<div className="flex items-center gap-2">
 				<h1 className="text-3xl font-semibold">{generatedContent.title}</h1>
-		<ContentViewer 
-			generatedContent={generatedContent}
-			key={generatedContent.id}
-			onSave={handleSave}
-		/>
-	</div>
+				<div className="flex gap-1">
+					{Array(5)
+						.fill(0)
+						.map((_,index)=>(
+						<StarIcon key={index} 
+						onClick={()=>handleRateChange(index+1)} 
+							className={clsx('w-8 h-8 cursor-pointer', 
+							(generatedContent.rate || 0) > index && "fill-black")}/>
+					))}
+				</div>
+			</div>
+			<ContentViewer 
+				generatedContent={generatedContent}
+				key={generatedContent.id}
+				onSave={handleSave}
+			/>
+		</div>
+	)
 }
